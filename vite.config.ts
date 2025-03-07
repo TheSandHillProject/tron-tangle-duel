a4f3ab2f8ae318b4024a66a88f5653e45e30ef34
nicpavao <mail@nicpavao.com>
2025-03-07 13:38:08 -0600

    changed the base in the vite.config.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/tron-tangle-duel/" : "/", 
+  base: "./",
   server: {
     host: "::",
     port: 8080,
