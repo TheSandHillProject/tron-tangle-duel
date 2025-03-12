ed1fedf60cb22518f4895780f0cda7c28df95001
nicpavao <mail@nicpavao.com>
2025-03-12 11:15:55 -0500

    changed default FPS to 5

--- src/context/GameContext.tsx
+++ src/context/GameContext.tsx
@@ -18,7 +18,7 @@ export const GameProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [skipSetup, setSkipSetup] = useState(false);
   const [lastGameMode, setLastGameMode] = useState<'single' | 'two' | null>(null);
   const [navigatingFrom, setNavigatingFrom] = useState<string | null>(null);
-  const [savedFPS, setSavedFPS] = useState<number>(2); // Default FPS is 2
+  const [savedFPS, setSavedFPS] = useState<number>(5); // Default FPS is 5
 
   return (
     <GameContext.Provider value={{ 
